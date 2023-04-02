28cbeaa3a4b66119ba8b606b0cdaf76ea77ea0a2
Joel Kassim Moratalla <joelkasmor16@gmail.com>
2023-04-02 16:17:50 +0200

    Encrypt user password

--- server/src/user/controller.js
+++ server/src/user/controller.js
@@ -3,7 +3,8 @@ const bcrypt = require('bcrypt');
 module.exports = {
     signUp: async (req, res, next) => {
         try {
-            
+            const hashedPassword = await bcrypt.hash(req.body.password, 10);
+            const user = { name: req.body.name, password: hashedPassword }
         } catch (error) {
             next(error)
         }
